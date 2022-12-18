7335bfff4e8443cd62bb6bdc319c91b456e0f8fb
Harmdhast <pandemonium.harmdhast@gmail.com>
2022-12-18 20:12:14 +0100

    Test

--- build.ps1
+++ build.ps1
@@ -1,18 +1,18 @@
 #Requires -Version 7.0
 
-# $ToConvert = Get-ChildItem .\*.png -Recurse
+$ToConvert = Get-ChildItem .\recommendation\*.jpg -Recurse
 
-Select-String -Path .\*.html, .\recommendation\*.html -Pattern "\.jpg", "\.png"
+#Select-String -Path .\*.html, .\recommendation\*.html -Pattern "\.jpg", "\.png"
 
-$audioFiles = Get-ChildItem * -Recurse -Include *.mp3,*.wav,*.ogg -Exclude "*_0gb*"
+#$audioFiles = Get-ChildItem * -Recurse -Include *.mp3,*.wav,*.ogg -Exclude "*_0gb*"
 
-$audioFiles
+#$audioFiles
 
-# $ArgumentList = '-i "{0}" -c:v libwebp "{1}"' -f $_.FullName, $_.FullName.Replace("jpg","webp");
+#$ArgumentList = '-i "{0}" -c:v libwebp "{1}"' -f $_.FullName, $_.FullName.Replace("jpg","webp");
 #Start-Process ffmpeg -ArgumentList $ArgumentList -Wait -NoNewWindow
 #Select-String -Path *.html -Pattern $_.Name
 #$_.Length - (Get-Item -Path $_.FullName.Replace("jpg","webp")).Length
-# ForEach-Object {
-#     $ArgumentList = '-i "{0}" -c:v libwebp "{1}"' -f $_.FullName, $_.FullName.Replace("jpg","webp");
-#     Start-Process ffmpeg -ArgumentList $ArgumentList -Wait -NoNewWindow
-# }
+$ToConvert | ForEach-Object {
+     $ArgumentList = '-i "{0}" -c:v libwebp -vf scale=250:250 -pix_fmt yuva420p -lossless 0 -quality 80 "{1}"' -f $_.FullName, $_.FullName.Replace("jpg","webp");
+     Start-Process ffmpeg -ArgumentList $ArgumentList -Wait -NoNewWindow
+}
